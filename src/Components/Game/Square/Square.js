173dedf6d208b09f44e9ade0cdb1ec28de235404
erinruthmaness <erinruthmaness@yahoo.com>
2021-05-04 19:46:30 -0400

    I have no self-control. I added in long-press functionality for the flagging squares for mobile users

--- src/Components/Game/Square/Square.js
+++ src/Components/Game/Square/Square.js
@@ -1,20 +1,35 @@
-// import { useState } from 'react';
+import { useRef } from "react";
 import styles from "./Square.module.css";
 
+let count = 0;
+
 function Square(props) {
+  let whichClick = "left";
+  const countRef = useRef();
+
   const handleMouseDown = (event) => {
     console.log("clicked " + props.id);
+    //start counting how long they hold down the mouse
+    //(for mobile users who can't right-click)
+    countRef.current = setInterval(() => {
+      count = count + 1;
+      if (count > 0) {
+        props.changeFace(["0xD83D", "0xDE0A"]); //smiling
+      }
+    }, 400);
     if (event.nativeEvent.which === 1) {
       //left click
       if (!props.flagged) {
         props.changeFace(["0xD83D", "0xDE31"]); //yelling
       }
+    } else {
+      whichClick = "right";
     }
   };
 
   const handleMouseUp = (event) => {
-    let whichClick = "left";
-    if (event.nativeEvent.which !== 1) {
+    clearInterval(countRef.current);
+    if (event.nativeEvent.which !== 1 || count > 0) {
       whichClick = "right";
     }
     props.onClick(props.column, props.row, whichClick);
@@ -22,12 +37,16 @@ function Square(props) {
 
   return (
     <button
-      className={`${styles.grid_square} windows95 ${props.clicked && styles.clicked_square}`}
+      className={`${styles.grid_square} windows95 ${
+        props.clicked && styles.clicked_square
+      }`}
       onMouseDown={handleMouseDown}
       onMouseUp={handleMouseUp}
       onContextMenu={(e) => e.preventDefault()}
     >
-      {props.flagged ? String.fromCharCode(props.show[0], props.show[1]) : props.show}
+      {props.flagged
+        ? String.fromCharCode(props.show[0], props.show[1])
+        : props.show}
     </button>
   );
 }
